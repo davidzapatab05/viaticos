7bfe1f1bbeea1ac6d3739806fb2363c5cc183ad1
David Zapata <davidzapata@example.com>
2025-11-21 02:13:26 -0500

    Exclude manifest.json from middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -63,7 +63,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|icons).*)',
   ],
 }
 
